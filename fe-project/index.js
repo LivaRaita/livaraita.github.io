1f10b06d0495520ca1dc3658bc602a843e74eaab
Liva Raita <liva.raita@gmail.com>
2020-03-01 17:26:56 +0000

    project features

--- fe-project/index.js
+++ fe-project/index.js
@@ -2,6 +2,7 @@ let newProductButton = document.getElementById("new-product");
 let addNewProductPopup = document.getElementById("add-new-product");
 newProductButton.addEventListener("click", function() {
   addNewProductPopup.classList.add("overlay-full-screen-visible");
+  storeDiv.classList.add("form-item-hidden");
 });
 
 let products = [];
@@ -17,8 +18,17 @@ function hex(value) {
   return Math.floor(value).toString(16);
 }
 
-const addProduct = ev => {
+const addProduct = function(ev) {
   ev.preventDefault();
+
+  let inputRequired = document.getElementsByClassName("required");
+  for (let i = 0; i < inputRequired.length; i++) {
+    if (inputRequired[i].value === "none" || inputRequired[i].value === "") {
+      inputRequired[i].focus();
+      return false;
+    }
+  }
+
   let product = {
     product_id: objectId(),
     name: document.getElementById("name").value,
@@ -35,6 +45,14 @@ const addProduct = ev => {
   addProductToTable(product);
 
   localStorage.setItem("ProductList", JSON.stringify(products));
+  addNewProductPopup.classList.remove("overlay-full-screen-visible");
+
+  // let successPopup = document.getElementById("success");
+  // let displayProductName = document.getElementById("display-product-name");
+  successPopup.classList.add("overlay-visible");
+  displayProductName.innerHTML = product.name;
+
+  deleteItemPopup.classList.remove("overlay-visible");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -76,35 +94,25 @@ function addProductToTable(value) {
   $("#productListHTML").append(productListHTML);
 }
 
-// Get modal when user adds a product
+let deleteItemPopup = document.getElementById("delete-item");
+let successPopup = document.getElementById("success");
+let displayProductName = document.getElementById("display-product-name");
 
-let modalForSuccess = document.getElementById("success");
-let btnSubmit = document.getElementById("submit-product");
-// let closeButton = document.getElementById("close-btn");
-// let newProductPopup = document.getElementById("add-new-product");
+function openDeletePopup() {
+  deleteItemPopup.classList.toggle("overlay-visible");
+}
 
-// btnSubmit.onclick = function() {
-//   newProductPopup.style.display = "none";
-//   modalForSuccess.style.visibility = "visible";
-// };
+let closePopup = document.querySelectorAll(".close");
+for (var i = 0; i < closePopup.length; i++) {
+  closePopup[i].addEventListener("click", function() {
+    deleteItemPopup.classList.remove("overlay-visible");
+    successPopup.classList.remove("overlay-visible");
+    addNewProductPopup.classList.remove("overlay-full-screen-visible");
+  });
+}
 
 function initializeRemoveButtons() {
   let btnRemove = document.querySelectorAll(".remove");
-
-  let deleteItemPopup = document.getElementById("delete-item");
-
-  function openDeletePopup() {
-    // let deleteItemPopup = document.getElementById("delete-item");
-    deleteItemPopup.classList.toggle("overlay-visible");
-  }
-
-  let closePopup = document.querySelectorAll(".close");
-  for (var i = 0; i < closePopup.length; i++) {
-    closePopup[i].addEventListener("click", function() {
-      deleteItemPopup.classList.remove("overlay-visible");
-      addNewProductPopup.classList.remove("overlay-full-screen-visible");
-    });
-  }
 
   for (var i = 0; i < btnRemove.length; i++) {
     btnRemove[i].addEventListener("click", function() {
@@ -121,6 +129,8 @@ function initializeRemoveButtons() {
             products.splice(i, 1);
             localStorage.setItem("ProductList", JSON.stringify(products));
             fillTable(products);
+            deleteItemPopup.classList.remove("overlay-visible");
+            initializeRemoveButtons();
           }
         }
       });
@@ -128,31 +138,33 @@ function initializeRemoveButtons() {
   }
 }
 
-//TODO: Add form validation with if statements
+let sell = document.getElementById("sell");
+let storeDiv = document.getElementById("storeDiv");
+let storeSelection = document.getElementById("store");
 
-// let deleteItemPopup = document.getElementsByClassName("overlay");
-// deleteItemPopup.classList.toggle("overlay:target");
-// });
-
-// btnRemove.onclick = function() {
-//   let deleteItemPopup = document.getElementsByClassName("overlay");
-//   deleteItemPopup.classList.toggle("overlay:target");
-// };
-
-// TODO: Here I need to add a product name and
-// later remove it from this popup
+sell.addEventListener("change", function() {
+  if (sell.checked) {
+    sell.value = "yes";
+    storeDiv.classList.remove("form-item-hidden");
+    storeSelection.classList.add("required");
+  } else {
+    sell.value = "no";
+    storeDiv.classList.add("form-item-hidden");
+    storeSelection.classList.remove("required");
+  }
+});
 
 //Task description:
 
 // DONE 1. Create a button that shows a form (it can slid from the side, be shown in the main html or as a popup)
-// UN-BUG Add minimum 4 different form fields, make one be visible only on some type of conditions (if one form field has been filled, checkbox selected, select field appropriate value selected)
-// Validate minimum two of the fields (on button click point nr.4), show a visual representation if fields ar not valid
+// DONE Add minimum 4 different form fields, make one be visible only on some type of conditions (if one form field has been filled, checkbox selected, select field appropriate value selected)
+// DONE Validate minimum two of the fields (on button click point nr.4), show a visual representation if fields ar not valid
 // Error messages for not valid fields will be treated as a bonus
 // DONE Add a button that triggers the validation
 // DONE If you find out a way how to save the data somewhere - it will be treated as a bonus
-// If Validation passes show a popup with a full screen backdrop that contains some type of message with some data from the form (like: "thank you, username, we have received your request")
-// Add a button to the popup that closes it
-// Create a table where the data from the form will be shown, add an ID column and actions column that contains a delete button
+// DONE If Validation passes show a popup with a full screen backdrop that contains some type of message with some data from the form (like: "thank you, username, we have received your request")
+// DONE Add a button to the popup that closes it
+// DONE Create a table where the data from the form will be shown, add an ID column and actions column that contains a delete button
 // // If you did not think about a way to save the data, create 10 fake static records
-// // If you saved the data somewhere, show it dynamically, make an action that can delete each individual record
+// // DONE If you saved the data somewhere, show it dynamically, make an action that can delete each individual record
 // Push the final product to github and post it in google classroom under the assignment
